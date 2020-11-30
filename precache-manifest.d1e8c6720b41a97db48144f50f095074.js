493914b47f45a0703cce826b783f064df71ad79e
unknown <sandeep.misra96@gmail.com>
2020-11-30 16:39:48 +0530

    Updates

---
+++ precache-manifest.d1e8c6720b41a97db48144f50f095074.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bcbf9af7854958174698cb739bd9f01",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "2dae0c5a4a5effcc6c62",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "1ebc6cc908a8060f30f6",
+    "url": "/covid-tracker/static/css/main.575429ae.chunk.css"
+  },
+  {
+    "revision": "2dae0c5a4a5effcc6c62",
+    "url": "/covid-tracker/static/js/2.37361bae.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-tracker/static/js/2.37361bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ebc6cc908a8060f30f6",
+    "url": "/covid-tracker/static/js/main.d3b35c13.chunk.js"
+  },
+  {
+    "revision": "1af475054be274cc1e5c",
+    "url": "/covid-tracker/static/js/runtime-main.9cfae146.js"
+  }
+]);
